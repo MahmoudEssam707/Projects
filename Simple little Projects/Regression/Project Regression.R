a570cb44dd55de7362ade32178d69ba1e11acb78
Mahmoud Essam <mahmoudessam0123@gmail.com>
2023-05-12 22:35:36 +0300

    New things for Regression project "MLR"

--- Simple little Projects/Regression/Project Regression.R
+++ Simple little Projects/Regression/Project Regression.R
@@ -1,13 +1,5 @@
 # Write your model
-# Simple linear regression
-# Check list
-# SXX,Syy,Sxy(Checked)
-# Beta 1,Beta0(Checked)
-# SSR,SSE(Checked)
-# Anova table(Checked)
-# Confidence interval for estimator at given significant level(Checked)
-# Calculation of C.l. for mean response and new observation(Checked)
-# Scatter plot contain fitted line(Checked)
+# Simple linear regression vs Multiple Linear Regression
 SLR <- function(Data){
   # Getting needed data
   X <- Data$x
@@ -20,12 +12,12 @@ SLR <- function(Data){
   Beta_1 <- SXy / SXX
   Beta_0 <- mean(y) - Beta_1*mean(X)
   # Calculating Sum squares (Regression,error,Total)
-  SST <- round(Syy,digits = 2)
-  SSR <- round(Beta_1*SXy,digits = 2)
+  SST <- Syy
+  SSR <- Beta_1*SXy
   SSE <- SST - SSR
   # Calculating Mean squares (Regression,error,Total)
   MSR = SSR/1
-  MSE = round(SSE/(length(X)-2),2)
+  MSE = (SSE/(length(X)-2))
   MST = MSR+MSE
   # Draw Anova Table
   anova_table <- data.frame(
@@ -33,64 +25,128 @@ SLR <- function(Data){
     SS = c(SSR,SSE,SST),
     DOF = c(1,length(X)-2,length(X)-1),
     MS = c(MSR,MSE,""),
-    F0 = c(round(MSR/MSE,2),"","")
-  )
+    F0 = c(MSR/MSE,"",""))
+  # Calculate the Value of R^2
+  R_squared <- SSR / SST
+  direction <- ifelse(Beta_1 < 0, "negative", "positive")
+  # Generate output text
   # CI for Estimators
   SL = as.numeric(readline("PLease choose your significance level : "))
   if(length(X)>=30){  
     # CI for BETA 1 using Z
-    Upper_Beta_1 = round(Beta_1 + qnorm(SL/2, mean = 0, sd = 1, lower.tail=F)*sqrt(MSE/SXX),2)
-    Lower_Beta_1 = round(Beta_1 - qnorm(SL/2, mean = 0, sd = 1, lower.tail=F)*sqrt(MSE/SXX),2)
+    Upper_Beta_1 = Beta_1 + qnorm(SL/2, mean = 0, sd = 1, lower.tail=F)*sqrt(MSE/SXX)
+    Lower_Beta_1 = Beta_1 - qnorm(SL/2, mean = 0, sd = 1, lower.tail=F)*sqrt(MSE/SXX)
     # CI for BETA 0 using Z
-    Upper_Beta_0 = round(Beta_0 + qnorm(SL/2, mean = 0, sd = 1, lower.tail=F)*sqrt(MSE*((1/length(X)) + mean(X)^2/SXX)),2)
-    Lower_Beta_0 = round(Beta_0 - qnorm(SL/2, mean = 0, sd = 1, lower.tail=F)*sqrt(MSE*((1/length(X)) + mean(X)^2/SXX)),2)
+    Upper_Beta_0 = Beta_0 + qnorm(SL/2, mean = 0, sd = 1, lower.tail=F)*sqrt(MSE*((1/length(X)) + mean(X)^2/SXX))
+    Lower_Beta_0 = Beta_0 - qnorm(SL/2, mean = 0, sd = 1, lower.tail=F)*sqrt(MSE*((1/length(X)) + mean(X)^2/SXX))
   }else{
     # CI for BETA 1 using T
-    Upper_Beta_1 = round(Beta_1 + qt(SL/2,df = length(X)-2, lower.tail=F)*sqrt(MSE/SXX),2)
-    Lower_Beta_1 = round(Beta_1 - qt(SL/2,df = length(X)-2, lower.tail=F)*sqrt(MSE/SXX),2)
+    Upper_Beta_1 =  Beta_1 + qt(SL/2,df = length(X)-2, lower.tail=F)*sqrt(MSE/SXX)
+    Lower_Beta_1 =  Beta_1 - qt(SL/2,df = length(X)-2, lower.tail=F)*sqrt(MSE/SXX)
     # CI for BETA 0 using T
-    Upper_Beta_0 = round(Beta_0 + qt(SL/2,df = length(X)-2, lower.tail=F)*sqrt(MSE*((1/length(X)) + mean(X)^2/SXX)),2)
-    Lower_Beta_0 = round(Beta_0 - qt(SL/2,df = length(X)-2, lower.tail=F)*sqrt(MSE*((1/length(X)) + mean(X)^2/SXX)),2)
+    Upper_Beta_0 =  Beta_0 + qt(SL/2,df = length(X)-2, lower.tail=F)*sqrt(MSE*((1/length(X)) + mean(X)^2/SXX))
+    Lower_Beta_0 =  Beta_0 - qt(SL/2,df = length(X)-2, lower.tail=F)*sqrt(MSE*((1/length(X)) + mean(X)^2/SXX))
   }
   # CI for Mean Response and New Obs.
-  X0 <- as.numeric(readline("Please Choose the value of X0 :"))
+  X0 <- as.numeric(readline("Please Choose the value of X0 : "))
   Y0 <- Beta_0 + Beta_1*X0
   if(length(X)>=30){  
     # CI for Y0 mean response using Z
-    Y0_mean_reponse_upper = round(Y0 + qnorm(SL/2, mean = 0, sd = 1, lower.tail=F)*sqrt(MSE*(1/length(X) + ((X0-mean(X))^2/SXX))),2)
-    Y0_mean_reponse_lower = round(Y0 - qnorm(SL/2, mean = 0, sd = 1, lower.tail=F)*sqrt(MSE*(1/length(X) + ((X0-mean(X))^2/SXX))),2)
+    Y0_mean_reponse_upper = Y0 + qnorm(SL/2, mean = 0, sd = 1, lower.tail=F)*sqrt(MSE*(1/length(X) + ((X0-mean(X))^2/SXX)))
+    Y0_mean_reponse_lower = Y0 - qnorm(SL/2, mean = 0, sd = 1, lower.tail=F)*sqrt(MSE*(1/length(X) + ((X0-mean(X))^2/SXX)))
     # CI for Y0 New obs. using Z
-    Y0_new_obs_upper = round(Y0 + qnorm(SL/2, mean = 0, sd = 1, lower.tail=F)*sqrt(MSE*(1 + 1/length(X) + ((X0-mean(X))^2/SXX))),2)
-    Y0_new_obs_lower = round(Y0 - qnorm(SL/2, mean = 0, sd = 1, lower.tail=F)*sqrt(MSE*(1 + 1/length(X) + ((X0-mean(X))^2/SXX))),2)
+    Y0_new_obs_upper = Y0 + qnorm(SL/2, mean = 0, sd = 1, lower.tail=F)*sqrt(MSE*(1 + 1/length(X) + ((X0-mean(X))^2/SXX)))
+    Y0_new_obs_lower = Y0 - qnorm(SL/2, mean = 0, sd = 1, lower.tail=F)*sqrt(MSE*(1 + 1/length(X) + ((X0-mean(X))^2/SXX)))
   } else{
     # CI for Y0 mean response using T
-    Y0_mean_reponse_upper = round(Y0 + qt(SL/2,df = length(X)-2, lower.tail=F)*sqrt(MSE*(1/length(X) + ((X0-mean(X))^2/SXX))),2)
-    Y0_mean_reponse_lower = round(Y0 - qt(SL/2,df = length(X)-2, lower.tail=F)*sqrt(MSE*(1/length(X) + ((X0-mean(X))^2/SXX))),2)
+    Y0_mean_reponse_upper = Y0 + qt(SL/2,df = length(X)-2, lower.tail=F)*sqrt(MSE*(1/length(X) + ((X0-mean(X))^2/SXX)))
+    Y0_mean_reponse_lower = Y0 - qt(SL/2,df = length(X)-2, lower.tail=F)*sqrt(MSE*(1/length(X) + ((X0-mean(X))^2/SXX)))
     # CI for Y0 New obs. using T
-    Y0_new_obs_upper = round(Y0 + qt(SL/2,df = length(X)-2,lower.tail=F)*sqrt(MSE*(1 + 1/length(X) + ((X0-mean(X))^2/SXX))),2)
-    Y0_new_obs_lower = round(Y0 - qt(SL/2,df = length(X)-2,lower.tail=F)*sqrt(MSE*(1 + 1/length(X) + ((X0-mean(X))^2/SXX))),2)
+    Y0_new_obs_upper = Y0 + qt(SL/2,df = length(X)-2,lower.tail=F)*sqrt(MSE*(1 + 1/length(X) + ((X0-mean(X))^2/SXX)))
+    Y0_new_obs_lower = Y0 - qt(SL/2,df = length(X)-2,lower.tail=F)*sqrt(MSE*(1 + 1/length(X) + ((X0-mean(X))^2/SXX)))
   }
-  # Print function
-  cat("Value of SXX is",SXX
-      ,"\nValue of SYY is",Syy
-      ,"\nValue of SXy is",SXy
-      ,"\nValue of Beta One(slope) is",Beta_1
-      ,"\nValue of Beta node(intercept) is",Beta_0
-      ,"\nValue of SST =",Syy
-      ,"\nValue of SSR =",SSR
-      ,"\nValue of SSE =",SSE
-      ,"\n*and here's Anova Table* \n")
+  # put all this text in lonely file
+  # Print summary statistics
+  cat("Summary statistics:\n"
+ ,"  SXX: ", SXX, "\n"
+ ,"  SYY: ", Syy, "\n"
+ ,"  SXy: ", SXy, "\n"
+ ,"  Beta One (slope): ", Beta_1, "\n"
+ ,"  Beta node (intercept): ", Beta_0, "\n"
+ ,"  SST: ", Syy, "\n"
+ ,"  SSR: ", SSR, "\n"
+ ,"  SSE: ", SSE, "\n"
+ ,"  R-squared: ", round(R_squared * 100, 2), "% in the ", direction, " direction.\n")
+  # Print ANOVA table
+  cat("ANOVA table:\n",file="output.txt")
   print(anova_table)
-  cat("The CI for BETA One :",Lower_Beta_1,"< B1 <",Upper_Beta_1,
-      "\nThe CI for BETA Node :",Lower_Beta_0,"< B0 <",Upper_Beta_0)
-  cat("\nThe CI for Y0 Mean Resp. :",Y0_mean_reponse_lower,"< Y0 <",Y0_mean_reponse_upper,
-      "\nThe CI for Y0 New obs. :",Y0_new_obs_lower,"< Y0 <",Y0_new_obs_upper)
-  plot(X,y)
-  abline(a=Beta_0,b=Beta_1)
+  
+  # Print confidence intervals
+  cat("\nConfidence intervals:\n"
+ ,"  CI for Beta One: ", Lower_Beta_1, " < B1 < ", Upper_Beta_1, "\n"
+ ,"  CI for Beta Node: ", Lower_Beta_0, " < B0 < ", Upper_Beta_0, "\n"
+ ,"  CI for Y0 Mean Response: ", Y0_mean_reponse_lower, " < Y0 < ", Y0_mean_reponse_upper, "\n"
+ ,"  CI for Y0 New Observation: ", Y0_new_obs_lower, " < Y0 < ", Y0_new_obs_upper, "\n")
+  
+  # Plot regression line
+  plot(X, y)
+  abline(a = Beta_0, b = Beta_1, col = "red", lwd = 3)
 }
 MLR <- function(Data){
-  print("Not available for now")
+  # Constructing needed data
+  my_df <- as.matrix(data)
+  big_x <- cbind(b0=rep(1,length(data)),my_df)
+  x <- big_x[,-ncol(big_x)]
+  y <- subset(my_df, select = ncol(my_df))
+  xt <- t(x)
+  xtx <- xt%*%x
+  xtx_inverse <- solve(xtx)
+  xty <- xt%*%y
+  # OPERATIONS FOR SS
+  betas <- xtx_inverse%*%xt%*%y
+  y_bar <- mean(y)
+  betas_t <- t(betas)
+  # SUM SQUARES
+  SSE<- (t(y)%*%y)-(betas_t%*%xt%*%y) 
+  # Calaulate SST
+  SST<-(t(y)%*%y)-(length(y))*(y_bar)^2    
+  # CAlculate SSR
+  SSR=SST-SSE               
+  R_square <- SSR/SST
+  #ANOV
+  k <- ncol(big_x[, -c(1, ncol(big_x))])
+  p <- k+1
+  MSR = SSR/k
+  MSE = SSE/(length(y)-p)
+  MST = MSR+MSE
+  # Draw Anova Table
+  anova_table <- data.frame(
+    row.names =  c("Regression", "Error", "Total"),
+    SS = c(SSR,SSE,SST),
+    DOF = c(k,length(y)-p,p),
+    MS = c(MSR,MSE,""),
+    F0 = c(MSR/MSE,"",""))
+  # Calculate the Value of R^2
+  R_squared <- SSR / SST
+  cat("Summary statistics:\n"
+      ,"  Matrix of X: ", x, "\n"
+      ,"  Matrix of y: ", y, "\n"
+      ,"  Matrix of (XtX): ", xtx, "\n"
+      ,"  Matrix of C (XtX)^-1: ", xtx_inverse, "\n"
+      ,"  Beta values: ", betas, "\n"
+      ,"  SST: ", SST, "\n"
+      ,"  SSR: ", SSR, "\n"
+      ,"  SSE: ", SSE, "\n"
+      ,"  R-squared: ", round(R_squared * 100, 2))
+  # Print ANOVA table
+  cat('ANOVA table')
+  print(anova_table)
 }
-path <- readline("Please put your path : ")
+path <- noquote(file.choose())
 Data <- read.csv(path)
-SLR(Data)
+value_of_regression <- as.numeric(readline("Which type of method do you need? :\n1-SLR(Simple Linear Regression)\n2-MLR(Multiple Linear Regression)"))
+Function <- switch(value_of_regression,
+                   SLR = SLR(Data),
+                   MLR = MLR(Data),
+                   stop("Invalid input. Please enter 1 or 2."))
+print(Function)
